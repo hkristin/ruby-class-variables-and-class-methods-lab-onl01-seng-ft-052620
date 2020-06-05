6e31741063fd0dd963cf7ccdc4b29eff0db6375c
Kristin Howard <hkristin@andrew.cmu.edu>
2020-06-05 23:42:02 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,11 +1,13 @@
 class Song
-  @@album_count = 0
+  def new(name,artist,genre)
+  end
+  
+  @@song_count = 0
   def initialize
     @@album_count
   end
   def self.count
     @@album_count += 1
   end
-Album.new
 end
 
